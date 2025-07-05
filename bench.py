8c815471a86b70c121e4b89854a61cc19f2450dd
imkero <kerorek@outlook.com>
2025-07-06 06:00:42 +0800

    chore: same nano-vllm and vllm usage

--- bench.py
+++ bench.py
@@ -19,8 +19,6 @@ def main():
     for tokens in prompt_token_ids:
         requests.append(dict(prompt_token_ids=tokens))
     sampling_params = [SamplingParams(temperature=0.6, ignore_eos=True, max_tokens=randint(100, max_ouput_len)) for _ in range(num_seqs)]
-    # uncomment the following line for vllm
-    # prompt_token_ids = [dict(prompt_token_ids=p) for p in prompt_token_ids]
 
     llm.generate(["Benchmark: "], SamplingParams())
     t = time.time()
