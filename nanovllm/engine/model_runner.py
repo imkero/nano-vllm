b44562fecb67ea9ef275247aa36f0085f2c6f432
imkero <kerorek@outlook.com>
2025-07-06 10:57:53 +0800

    feat: dynamic load model class

--- nanovllm/engine/model_runner.py
+++ nanovllm/engine/model_runner.py
@@ -7,8 +7,6 @@ from multiprocessing.shared_memory import SharedMemory
 
 from nanovllm.config import Config
 from nanovllm.engine.sequence import Sequence
-from nanovllm.models.qwen3 import Qwen3ForCausalLM
-from nanovllm.models.qwen2 import Qwen2ForCausalLM
 from nanovllm.layers.sampler import Sampler
 from nanovllm.utils.context import set_context, get_context, reset_context
 from nanovllm.utils.loader import load_model
@@ -30,13 +28,15 @@ class ModelRunner:
         default_dtype = torch.get_default_dtype()
         torch.set_default_dtype(hf_config.torch_dtype)
         torch.set_default_device("cuda")
-        arch_type = getattr(hf_config, "model_type", None)
-        if arch_type == "qwen3":
+        model_type = getattr(hf_config, "model_type", None)
+        if model_type == "qwen3":
+            from nanovllm.models.qwen3 import Qwen3ForCausalLM
             self.model = Qwen3ForCausalLM(hf_config)
-        elif arch_type == "qwen2":
+        elif model_type == "qwen2":
+            from nanovllm.models.qwen2 import Qwen2ForCausalLM
             self.model = Qwen2ForCausalLM(hf_config)
         else:
-            raise ValueError(f"Unsupported architecture: {arch_type}")
+            raise ValueError(f"Unsupported architecture: {model_type}")
         load_model(self.model, config.model)
         self.sampler = Sampler()
         self.warmup_model()
